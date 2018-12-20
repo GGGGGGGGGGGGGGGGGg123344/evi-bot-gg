2b682fd1901ab65a47baf0eb00b708df6329dc84
Evil-Op <46014065+Evil-Op@users.noreply.github.com>
2018-12-20 11:00:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,12 +41,29 @@ client.on('guildMemberAdd', member => {
         .setColor('RANDOM')
         .setThumbnail(memberavatar)
         .addField(':bust_in_silhouette: | name : ', `${member}`)
-        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
+        .addField(':microphone2: | Welcome!', `EvilOP server aapka bohat bohat swagat karta he :metal:, ${member}`)
         .addField(':id: | User :', "**[" + `${member.id}` + "]**")
-        .addField(':family_mwgb: | Your are the member', `${member.guild.memberCount}`)
-        .addField("Name", `<@` + `${member.id}` + `>`, true)
+        .addField(':family_mwgb: | You are the member number...', `${member.guild.memberCount}`)
+        .addField("Aapka naam", `<@` + `${member.id}` + `>`, true)
         .addField('Server', `${member.guild.name}`, true )
         .setFooter(`**${member.guild.name}**`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+client.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'welcome-leave');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Name:', `${member}`)
+        .addField('Has Let the Server', ';(')
+        .addField('Bye Bye :(', 'Yaad teri aayegi...Hamko bada satayegi!!')
+        .addField('The server now as', `${member.guild.memberCount}` + " members")
+        .setFooter(`**${member.guild.name}`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
