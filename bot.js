26a501e91ef690766162cb5b39bba0241409c1d1
Evil-Op <46014065+Evil-Op@users.noreply.github.com>
2019-04-04 17:19:19 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ client.on("message", async message => {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Staff", "HOUND", "Mod", "LEADER"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Administrator", "MODERATOR", "Staff", "HOUND", "Mod", "LEADER"].includes(r.name)) )
       return message.reply("***Zinge tu nai kar sakta Noobde***");
     
     // Let's first check if we have a member and if we can kick them!
@@ -143,7 +143,7 @@ client.on("message", async message => {
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator", "Staff", "HOUND", "Mod", "LEADER", "Admin", "OWNER"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Administrator", "Staff", "HOUND", "MODERATOR", "LEADER", "Admin", "OWNER"].includes(r.name)) )
       return message.reply("***Zinge Tu nai kar sakta***");
     
     let member = message.mentions.members.first();
