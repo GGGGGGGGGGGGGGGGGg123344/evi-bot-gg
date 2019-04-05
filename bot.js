99cd60a51f49eb727f10c550382a99a1c232e4a3
Evil-Op <46014065+Evil-Op@users.noreply.github.com>
2019-04-05 15:02:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,19 +18,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`With Legs`);
+  client.user.setActivity(`for any help contact Capt.som`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`With Legs`);
+  client.user.setActivity(`for any help contact Capt.som`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`With Legs`);
+  client.user.setActivity(`for any help contact Capt.som`);
 });
 
 client.on('guildMemberAdd', member => {
